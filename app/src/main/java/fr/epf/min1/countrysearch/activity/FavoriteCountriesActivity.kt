0dc2a3f8de0d5e7215609981902400032e4cd9c6
Margaux Blondel <margaux.blondel@free.fr>
2024-06-09 14:04:13 +0200

    feat(activity): add refresh and sync buttons on favorite page

--- app/src/main/java/fr/epf/min1/countrysearch/activity/FavoriteCountriesActivity.kt
+++ app/src/main/java/fr/epf/min1/countrysearch/activity/FavoriteCountriesActivity.kt
@@ -1,29 +1,50 @@
 package fr.epf.min1.countrysearch.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import fr.epf.min1.countrysearch.MainActivity
 import fr.epf.min1.countrysearch.R
-import fr.epf.min1.countrysearch.data.Country
+import fr.epf.min1.countrysearch.data.click
 import fr.epf.min1.countrysearch.localstorage.AppDataBase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class FavoriteCountriesActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite_countries)
 
-        val recyclerView =
-            findViewById<RecyclerView>(R.id.favorite_countries_recyclerview)
-
+        // set favorite countries on page
+        recyclerView =
+            findViewById(R.id.favorite_countries_recyclerview)
         recyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        tryFetchFavoriteCountries()
 
+        // refresh button
+        setRefreshButton()
+
+        // home button
+        setHomeButton()
+    }
+
+    private fun setRefreshButton() {
+        val refreshButton = findViewById<ImageButton>(R.id.favorite_countries_refresh_imagebutton)
+        refreshButton.click {
+            tryFetchFavoriteCountries()
+        }
+    }
+
+    private fun tryFetchFavoriteCountries() {
         val db = Room.databaseBuilder(
             applicationContext,
             AppDataBase::class.java, "favorite-countries-db"
@@ -34,6 +55,14 @@ class FavoriteCountriesActivity : AppCompatActivity() {
             runOnUiThread {
                 recyclerView.adapter = ListCountryAdapter(countries)
             }
+        }
+    }
+
+    private fun setHomeButton() {
+        val homeButton = findViewById<ImageButton>(R.id.favorite_countries_home_imagebutton)
+        homeButton.click {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
